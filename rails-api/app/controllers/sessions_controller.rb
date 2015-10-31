73430f66cd5972ee857f5eff41e087fd2e220ac5
jeffreybiles <bilesjeffrey@gmail.com>
2015-10-31 12:40:14 -0500

    replacing login and logout

--- rails-api/app/controllers/sessions_controller.rb
+++ rails-api/app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
+    user = User.find_by(email: params[:username])
     if user.try(:authenticate, params[:password])
       render json: {user_id: user.id, authentication_token: user.authentication_token}
     else
--- rails-api/config/routes.rb
+++ rails-api/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :users
   resources :team_memberships
   resources :sessions, only: [:create]
+  post 'token' => 'sessions#create'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
